d9110564603ce0541d612806302a2247a40fcd5f
tarun-tatakuntala <tarunyt6@gmail.com>
2023-08-13 21:48:31 +0100

    Changes

---
+++ 5. Trees/Binary Search Tree Contains.py
@@ -0,0 +1,10 @@
+def contains(self, value):
+    temp = self.root
+    while temp is not None:
+        if value < temp.value:
+            temp = temp.left
+        elif value > temp.value:
+            temp = temp.right
+        else:
+            return True    
+    return False
