c0c3702bae497eb929c0c5e657bdcad7245df4f8
tarun-tatakuntala <tarunyt6@gmail.com>
2023-08-13 23:39:07 +0100

    commit

--- 5. Trees/Binary Search Tree Insert.py
+++ 5. Trees/Binary Search Tree Insert.py
@@ -7,31 +7,26 @@ class Node:
 class BinarySearchTree:
     def __init__(self):
         self.root = None
-    
-    def insert(self, value):
+
+    def Insert(self, value):
         new_node = Node(value)
-        if self.root is None:           # If there is no items in the tree, then new node becomes root
+        if self.root is None:
             self.root = new_node
             return True
         temp = self.root
         while(True):
-            if new_node.value == temp.value:    #If the new node is duplicate, then execution terminates
+            if new_node.value == temp.value:
                 return False
             if new_node.value < temp.value:
                 if temp.left is None:
                     temp.left = new_node
                     return True
                 temp = temp.left
-            else:
-                if temp.right is None:
-                    temp.right = new_node
-                    return True
-                temp = temp.right
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.Insert(2)
+my_tree.Insert(1)
+my_tree.Insert(3)
 
 print(my_tree.root.value)
 print(my_tree.root.left.value)
