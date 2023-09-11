fe0c3e66370673670de937833afa6b8527e27421
tarun-tatakuntala <tarunyt6@gmail.com>
2023-09-11 19:03:53 +0100

    commit

--- 10. Sorting/Insertion_Sort.py
+++ 10. Sorting/Insertion_Sort.py
@@ -2,7 +2,7 @@ def insertion_sort(array):
     for i in range(1,len(array)):
         temp =  array[i]
         j = i-1
-        while temp < array[j] and j > -1:
+        while j > -1 and temp < array[j]:
             array[j+1] = array[j]
             array[j] = temp
             j -=1
