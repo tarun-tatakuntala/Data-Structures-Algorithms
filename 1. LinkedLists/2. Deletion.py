ba90169124821ba3816e8698ceb80cba15fb9808
tarun-tatakuntala <tarunyt6@gmail.com>
2023-09-22 20:12:00 +0100

    commit

--- 1. LinkedLists/2. Deletion.py
+++ 1. LinkedLists/2. Deletion.py
@@ -6,3 +6,53 @@ Deletion also has 3 key operations. They are:
 3. Deletion at a specific position
 
 """
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1
+        return True
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while (temp.next) is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:    #If there is only one item and that item is removed, then head & tail are assigned nOne
+            self.head = None
+            self.tail = None
+        return temp
+    
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.pop()
+my_linked_list.print_list()
